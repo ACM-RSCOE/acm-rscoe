6e2c89759682fbdb2c9df72c7faf9635b1f22cc5
unknown <abhishekrdhanke636@gmail.com>
2022-03-13 22:00:49 +0530

    some content added

--- src/Components/Pages/Team.js
+++ src/Components/Pages/Team.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useEffect } from 'react'
 import Profile from '../Profile'
+import Aos from 'aos'
+import "aos/dist/aos.css"
 
 const Team = () => {
+  useEffect(() => [
+    Aos.init({duration:2000})
+  ],[])
   return (
       <>
     <div className='teamheader'><p>The Team</p>
+    <div className='staff-div'>
+      <div>
+    <img className="staff" data-aos="fade-up" src='./Images/team/principal.jpeg'></img>
+    <p className='name-staff'>Dr.R.K.Jain</p>
+    <p className='post-staff'>Director & Principal</p>
+    </div>
+    <div>
+    <img className="staff" data-aos="fade-up" src='./Images/team/hod.jpeg'></img>
+    <p className='name-staff'>Dr.S.V.Kedar</p>
+    <p className='post-staff'>Professor & HOD Computer Dept.</p>
+    </div>
+    <div>
+    <img className="staff" data-aos="fade-up" src='./Images/team/faculty.jpeg'></img>
+    <p className='name-staff'>Prof.S.N.Lohar</p>
+    <p className='post-staff'>Faculty Sponsor</p>
+    </div>
+    </div>
    </div>
     <Profile />
     
